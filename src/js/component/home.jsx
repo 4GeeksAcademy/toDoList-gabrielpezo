e6ce33ed232512a1d56c3ebdac273540515899e4
Gabriel Pezo Rojas <156217567+gabrielpezo@users.noreply.github.com>
2024-03-07 21:52:14 +0000

    toDo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,29 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [list, setList] = useState([]);
+	const [input, setInput] = useState("");
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<>	
+		<div className="backGround">
+			<div className="divCon container text-center">
+				<h1 className="title"><i>To-Do List</i></h1>
+				<ul className=" shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+					<li><input type="text" value={input} onChange={(e)=> setInput(e.target.value)} onKeyUp={(e) => {if (e.key === "Enter" ) {setList(list.concat(input)); setInput("")}}} placeholder="What do you need to do?"></input> </li>
+						{list.map ((item, index) => <li>
+						{item}<FontAwesomeIcon className="iconX" icon={faXmark} onClick={()=> setList (list.filter((item, currentIndex) => index != currentIndex))}/>
+					</li>)}
+				</ul>
+				<div className="counter d-flex justify-content-left"><strong>{list.length} tasks</strong></div>
+			</div>
 		</div>
+	</>		
 	);
 };
 
